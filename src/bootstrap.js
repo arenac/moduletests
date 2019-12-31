f4436557d7ff58410bbe8fc4a854169e71f86993
Nilo Neregato <neregato.nilo@gmail.com>
2019-12-31 02:47:55 +0100

    export instead and fix node_env

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,8 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV) {
+  process.env.NODE_ENV = process.env.NODE_ENV.trim();
+}
 
 dotenv.config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
